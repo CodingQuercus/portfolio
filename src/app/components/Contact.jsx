92338692a31dd09b50ace6d36ad3d75a0f78ce50
CodingQuercus <123453349+CodingQuercus@users.noreply.github.com>
2025-07-23 13:51:51 +0200

    more fixes

--- src/app/components/Contact.jsx
+++ src/app/components/Contact.jsx
@@ -40,7 +40,7 @@ export default function Contact() {
                     <div className="flex gap-3">
                         <motion.button
                             onClick={handleCopyEmail}
-                            className="bg-[#328e6e] text-[#f3f3f3] dark px-4 py-2 rounded-lg flex items-center gap-2 hover:bg-[#328e6e] cursor-pointer focus:outline-none focus:ring-4 focus:ring-[#328E6E] focus:border-[#328E6E]"
+                            className="bg-[#328e6e] text-[#f3f3f3] dark px-4 py-2 border-2 border-[#328e6e] rounded-lg flex items-center gap-2 hover:bg-[#328e6e90] cursor-pointer focus:outline-none focus:ring-4 focus:ring-[#328E6E] focus:border-[#328E6E]"
                             whileHover={{ scale: 1.05 }}
                             whileTap={{ scale: 0.95 }}
                         >
@@ -48,7 +48,7 @@ export default function Contact() {
                         </motion.button>
                         <motion.a
                             href={`mailto:${email}`}
-                            className="bg-[#328e6e] text-[#f3f3f3] px-4 py-2 rounded-lg flex items-center gap-2 hover:bg-[#328e6e] cursor-pointer focus:outline-none focus:ring-4 focus:ring-[#328E6E] focus:border-[#328E6E]"
+                            className="bg-[#328e6e] text-[#f3f3f3] px-4 py-2 border-2 border-[#328e6e] rounded-lg flex items-center gap-2 hover:bg-[#328e6e90] cursor-pointer focus:outline-none focus:ring-4 focus:ring-[#328E6E] focus:border-[#328E6E]"
                             whileHover={{ scale: 1.05 }}
                             whileTap={{ scale: 0.95 }}
                         >
@@ -65,7 +65,7 @@ export default function Contact() {
                     <p className="text-[#f3f3f3] dark:text-[#282828] text-sm mb-4">Connect with me!</p>
                     <motion.button
                         onClick={handleLinkedInRedirect}
-                        className="bg-[#328e6e] text-[#f3f3f3] px-4 py-2 rounded-lg flex items-center gap-2 cursor-pointer hover:bg-[#328e6e] focus:outline-none focus:ring-4 focus:ring-[#328E6E] focus:border-[#328E6E]"
+                        className="bg-[#328e6e] text-[#f3f3f3] px-4 py-2 border-2 border-[#328e6e] rounded-lg flex items-center gap-2 cursor-pointer hover:bg-[#328e6e90] focus:outline-none focus:ring-4 focus:ring-[#328E6E] focus:border-[#328E6E]"
                         whileHover={{ scale: 1.05 }}
                         whileTap={{ scale: 0.95 }}
                     >
