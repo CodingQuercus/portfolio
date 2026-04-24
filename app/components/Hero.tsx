9bdcb63442a46f5fee15db10ba301428adb61f9a
CodingQuercus <123453349+CodingQuercus@users.noreply.github.com>
2026-04-24 16:12:58 +0200

    några fixar med responsivitet

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -2,29 +2,29 @@
 
 export default function Hero() {
     return (
-        <section className="flex flex-col gap-2 md:gap-4 justify-center min-h-screen px-10 md:px-10 lg:px-0">
+        <section className="flex flex-col gap-2 md:gap-4 justify-center min-h-screen">
             <div className="flex flex-row items-center gap-1 md:gap-4 w-full">
-                <h1 className="text-5xl md:text-7xl lg:text-9xl font-bold">
+                <h1 className="text-4xl md:text-7xl lg:text-9xl font-bold">
                     Åke
                 </h1>
-                <span className="flex-1 rounded-full bg-foreground px-4 py-2 lg:px-24 lg:py-12">
+                <span className="flex-1 rounded-full bg-foreground px-2 py-2 sm:p-4 lg:px-12 lg:py-12">
                     <p className="font-bold text-background text-center text-sm md:text-2xl xl:text-3xl">
                         UX Designer
                     </p>
                 </span>
-                <span className="flex-1 rounded-full bg-foreground px-4 py-2 lg:px-24 lg:py-12">
+                <span className="flex-1 rounded-full bg-foreground px-2 py-2 sm:p-4 lg:px-12 lg:py-12">
                     <p className="font-bold text-background text-center text-sm md:text-2xl xl:text-3xl">
                         Frontend Dev
                     </p>
                 </span>
             </div>
             <div className="flex flex-row items-center gap-1 md:gap-4 w-full">
-                <span className="flex-1 rounded-full bg-foreground px-4 py-2 lg:px-24 lg:py-12">
+                <span className="flex-1 rounded-full bg-foreground px-2 py-2 sm:p-4 lg:px-12 lg:py-12">
                     <p className="font-bold text-background text-center text-sm md:text-2xl xl:text-3xl">
                         Umeå, Sweden
                     </p>
                 </span>
-                <h1 className="text-5xl md:text-7xl lg:text-9xl font-bold">
+                <h1 className="text-4xl md:text-7xl lg:text-9xl font-bold">
                     Flatholm
                 </h1>
             </div>
